6daa93e54c101bf6ad3809913825c4984f4eb044
GitHub Action <action@github.com>
2023-07-14 11:01:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/nuwak/demoplugin/MyBundle.kt
+++ src/main/kotlin/com/github/nuwak/demoplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.nuwak.demoplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/nuwak/demoplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/nuwak/demoplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.nuwak.demoplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
